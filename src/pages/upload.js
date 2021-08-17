34db049bd894f5b456def35d4ca89f8759a6e5f4
Anders Thrane Michelsen <atm@dbc.dk>
2021-08-17 14:51:48 +0200

    Prettify

--- src/pages/upload.js
+++ src/pages/upload.js
@@ -1,14 +1,14 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Upload() {
-    return (
-        <>
-            <h1>Upload</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
-        </>
-    )
+  return (
+    <>
+      <h1>Upload</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
 }
