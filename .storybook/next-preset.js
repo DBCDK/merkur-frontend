2f0fb61f906c88011dc6037b6eac4a6d8dd7bb83
Anders Thrane Michelsen <atm@dbc.dk>
2021-08-18 10:13:48 +0200

    Add more test framework
    Inspired by the node-nextjs-reference project
    
    Auditors: jbn, mib, jbr, hwha

---
+++ .storybook/next-preset.js
@@ -0,0 +1,62 @@
+/**
+ * @file
+ * This is a preset for storybook
+ * see https://storybook.js.org/docs/react/api/presets
+ *
+ * Basically, it modifies the webpack config to enable CSS modules.
+ */
+
+const path = require("path");
+
+module.exports = {
+    webpackFinal: async (baseConfig, options) => {
+        // Modify or replace config. Mutating the original reference object can cause unexpected bugs.
+        const { module = {} } = baseConfig;
+
+        const newConfig = {
+            ...baseConfig,
+            module: {
+                ...module,
+                rules: [...(module.rules || [])],
+            },
+        };
+        //
+        // CSS Modules
+        // Many thanks to https://github.com/storybookjs/storybook/issues/6055#issuecomment-521046352
+        //
+
+        // First we prevent webpack from using Storybook CSS rules to process CSS modules
+        newConfig.module.rules.find(
+            (rule) => rule.test.toString() === "/\\.css$/"
+        ).exclude = /\.module\.css$/;
+
+        // Then we tell webpack what to do with CSS modules
+        newConfig.module.rules.push({
+            test: /\.module\.css$/,
+            use: [
+                "style-loader",
+                {
+                    loader: "css-loader",
+                    options: {
+                        importLoaders: 1,
+                        modules: true,
+                    },
+                },
+            ],
+        });
+
+        // Adds svg loader to storybook webpack config
+        const fileLoaderRule = newConfig.module.rules.find(
+            (rule) => rule.test && rule.test.test(".svg")
+        );
+        fileLoaderRule.exclude = /\.svg$/;
+
+        newConfig.module.rules.push({
+            test: /\.svg$/,
+            enforce: "pre",
+            loader: require.resolve("@svgr/webpack"),
+        });
+
+        return newConfig;
+    },
+};
---
+++ e2e/cypress/support/commands.js
@@ -0,0 +1,36 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+/**
+ * Tabs function
+ * @param {int} n // n is number of tabs -> default to 1
+ *
+ */
+Cypress.Commands.add("tabs", (n = 1) => {
+  for (let i = 0; i < Number(n); i++) {
+    cy.tab();
+  }
+});
