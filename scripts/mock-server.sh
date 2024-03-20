8cfb32ec028dc2dc73e0c7b0c379eea7747e1f1c
Nikolaj Strands <nist@dbc.dk>
2024-03-20 10:02:27 +0100

    Fix wiremock docker image

--- scripts/mock-server.sh
+++ scripts/mock-server.sh
@@ -4,4 +4,4 @@ docker run -it --rm \
   -p 8080:8080 \
   --name wiremock \
   -v $PWD/wiremock:/home/wiremock \
-  docker-metascrum.artifacts.dbccloud.dk/wiremock:3.3.1
+  docker-externals.artifacts.dbccloud.dk/wiremock:3.3.1
