9d7e01fe6d539de536d34ae3a7026b8e062e61ef
jbr <jbr@dbc.dk>
2025-01-15 15:57:02 +0100

    Bump next

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,14 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/css/styles.css";
 
 export const parameters = {
-    backgrounds: {
-        default: "light",
+  backgrounds: {
+    default: "light",
+  },
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
-        },
-    },
+  },
 };
