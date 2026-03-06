2fb761009ee18df8afa2b71cff8808cea9b224e7
Jørgen G. Nielsen <jgn@dbc.dk>
2026-03-06 13:19:31 +0100

    add Ts in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 // eslint.config.mjs
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 import prettierConfig from "eslint-config-prettier/flat";
 
 export default defineConfig([
   ...nextVitals,
+  ...nextTs,
   prettierConfig,
   globalIgnores([
     ".next/**",
